da298df970b450b22b174a8cd90262f124d3185f
Lucas Moura Gonçalves <92659220+LucasMZ@users.noreply.github.com>
2022-09-29 13:39:31 -0300

    Index

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Header from './header';
+import reportWebVitals from './reportWebVitals';
+import Carrinho from './carrinho';
+import Footer from './Footer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <><><><React.StrictMode>
+    <App />
+  </React.StrictMode>
+    <Header></Header></>
+   </><Footer></Footer></>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
